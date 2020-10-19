c1d2e25bb60f1f077865a31643a55ee035ad67ad
codydps@gmail.com <codydps@gmail.com>
2020-10-19 03:10:37 -0500

    cert

--- dummycrt.sh
+++ dummycrt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out ./ssl/ctfd.crt -keyout ./ssl/ctfd.key
+openssl req -new -newkey rsa:4096 -x509 -sha256 -days 365 -nodes -out ./ssl/cert.pem -keyout ./ssl/privkey.pem
